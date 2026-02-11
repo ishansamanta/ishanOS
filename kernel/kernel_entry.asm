631a1ddb390bf049fd834cc1f8744b8747f6c2e8
Ishan Samanta <ishan07samanta@gmail.com>
2026-02-11 23:05:50 +0530

    Maintaining kernel_entry.asm file

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,7 +1,7 @@
 [bits 32]
-[extern _kernel_main] ; Tells NASM that the 'door' is in another file (kernel.c)
-                      ; Note the underscore! MinGW/Windows GCC needs it.
+[extern _kernel_main] 
+                     
 
 _start:
-    call _kernel_main ; Jump into your C code
-    jmp $             ; If the C code ever finishes, stay here forever
+    call _kernel_main 
+    jmp $             
